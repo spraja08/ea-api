2d8a25ea8c83b20826f7ce72c77af411b22907d5
Sp <rspamzn@a483e736f3f7.ant.amazon.com>
2020-07-13 07:23:40 +0800

    refactored

---
+++ src/main/java/com/amazonaws/entityanalytics/api/APIServer.java
@@ -0,0 +1,35 @@
+/**
+* REST API - Restlet main entry point
+*
+* @author  Raja SP
+*/
+
+package com.amazonaws.entityanalytics.api;
+
+import org.restlet.Component;
+import org.restlet.data.Protocol;
+
+public class APIServer {
+
+    public static void main(String[] args) throws Exception {
+    	if( args.length < 3 ) {
+    		System.err.println( "Usage : java -jar entity-analytics.jar <path to the resources> <entity360StoreIP> <entity360StorePort>" );
+    		return;
+    	}
+        Component component = new Component();
+        int port = 8111;
+        component.getServers().add(Protocol.HTTP, port);
+
+        component.getDefaultHost().attach("/api/v1", new APIApplication());
+
+        ElasticEntity360Store.elasticSearchIP = args[1];
+        ElasticEntity360Store.elasticSearchPort = Integer.parseInt( args[2]);
+
+        EA.initialise();
+        //initialse the application
+        ScriptsDef.initalise( args[ 0 ] );
+
+        // Start the component.
+        component.start();
+    }
+}
---
+++ src/main/java/com/amazonaws/entityanalytics/api/ScriptsDef.java
@@ -0,0 +1,153 @@
+/**
+* Holds the definitions of all building blocks and snippets 
+*
+* @author  Raja SP
+*/
+
+package com.amazonaws.entityanalytics.api;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.apache.http.HttpHost;
+import org.codehaus.commons.compiler.CompileException;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+public class ScriptsDef {
+
+    public static Map<String, Script> scriptsMap;
+
+    private static JsonObject adps;
+    private static JsonObject states;
+    private static JsonObject events;
+    private static JsonObject entities;
+
+    public static void initalise( String resourcePath ) throws IOException, CompileException {
+
+        String content = readFile( resourcePath + "/ADPsDef.json");
+        adps = (JsonObject) JsonParser.parseString(content);
+
+        content = readFile(resourcePath + "/StatesDef.json");
+        states = (JsonObject) JsonParser.parseString(content);
+
+        content = readFile( resourcePath + "/InputsDef.json");
+        events = (JsonObject) JsonParser.parseString(content);
+
+        content = readFile( resourcePath + "/EntitiesDef.json");
+        entities = (JsonObject) JsonParser.parseString(content);
+
+        IndexRequest request = new IndexRequest( "config" );
+        request.id( "adps" );
+        request.source( adps.toString(), XContentType.JSON );
+        IndexResponse indexResponse = ElasticEntity360Store.getClient().index(request, RequestOptions.DEFAULT);
+
+        request.id( "states" );
+        request.source( states.toString(), XContentType.JSON );
+        indexResponse = ElasticEntity360Store.getClient().index(request, RequestOptions.DEFAULT);
+
+        request.id( "events" );
+        request.source( events.toString(), XContentType.JSON );
+        indexResponse = ElasticEntity360Store.getClient().index(request, RequestOptions.DEFAULT);
+
+        request.id( "entities" );
+        request.source( entities.toString(), XContentType.JSON );
+        indexResponse = ElasticEntity360Store.getClient().index(request, RequestOptions.DEFAULT);
+
+        compileAllExpressions();
+    }
+
+    public static JsonObject get( String buildingBlock ) {
+        if( buildingBlock.equals( "adps" ) )
+            return adps;
+        else if( buildingBlock.equals( "states" ) )
+            return states;    
+        else if( buildingBlock.equals( "events" ) )
+            return events;  
+        else if( buildingBlock.equals( "entities" ) )
+            return entities;  
+        return null;    
+    }
+
+    public static JsonObject get( String buildingBlock, String id ) {
+        if( buildingBlock.equals( "adps" ) )
+            return adps.get( id ).getAsJsonObject();
+        else if( buildingBlock.equals( "states" ) )
+            return states.get( id ).getAsJsonObject();  
+        else if( buildingBlock.equals( "events" ) )
+            return events.get( id ).getAsJsonObject();
+        else if( buildingBlock.equals( "entities" ) )
+            return entities.get( id ).getAsJsonObject();  
+        return null; 
+    }
+
+    public static void add( String buildingBlock, String id, JsonObject value ) throws IOException {
+        JsonObject buildingBlocks = get( buildingBlock );
+        buildingBlocks.add( id, value );
+        IndexRequest request = new IndexRequest( "config" );
+        request.id( buildingBlock );
+        request.source( buildingBlocks.toString(), XContentType.JSON );
+        ElasticEntity360Store.getClient().index(request, RequestOptions.DEFAULT);
+    }
+
+    public static void compileAllExpressions() throws CompileException, IOException {
+        scriptsMap = new HashMap< String, Script >();
+        JsonObject adps = get("adps");
+        Set<String> keys = adps.keySet();
+        Iterator<String> itr = keys.iterator();
+        while (itr.hasNext()) {
+            String key = itr.next();
+            System.out.println( "compiling : " + key );
+            JsonObject jObj = adps.get(key).getAsJsonObject();
+            Script script = new Script( key, jObj.get("expression").getAsString(), jObj.get( "expressionType" ).getAsString() );
+            try {
+                script.initalise();
+                scriptsMap.put( key, script );
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        JsonObject states = get("states");
+        keys = states.keySet();
+        itr = keys.iterator();
+        while (itr.hasNext()) {
+            String key = itr.next();
+            JsonObject jObj = states.get(key).getAsJsonObject();
+            Script script = new Script( key, jObj.get("expression").getAsString(), "boolean" );
+            try {
+                script.initalise();
+                scriptsMap.put( key, script );
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String readFile( String fileName ) throws IOException {
+        InputStream is = new FileInputStream( fileName );
+        BufferedReader buf = new BufferedReader( new InputStreamReader( is ) );
+                
+        String line = buf.readLine();
+        StringBuilder sb = new StringBuilder();
+                
+        while( line != null ){
+           sb.append( line ).append( "\n" );
+           line = buf.readLine();
+        }
+        buf.close();
+        String fileAsString = sb.toString();
+        return fileAsString;
+    }
+
+}
